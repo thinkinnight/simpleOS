fd61b21678755ce00114701735f8a99469645405
cnyao <yaoqu@163.com>
2016-06-07 22:53:17 -0700

    add new segment in GDT

--- src/boot.asm
+++ src/boot.asm
@@ -9,6 +9,7 @@
 DA_32	equ 4000h
 DA_C	equ 98h
 DA_DRW	equ 92h
+DA_DRWA equ 93h
 
 ;org 07c00h	;load from 0000 7c00
 org 0100h	;load from 0000 0100
@@ -16,15 +17,45 @@ org 0100h	;load from 0000 0100
 
 [SECTION .gdt]
 LABEL_GDT:		Descriptor	0,	0,	0
+LABEL_DESC_NORMAL:	Descriptor	0, 0ffffh, DA_DRW
 LABEL_DESC_CODE32:	Descriptor	0,SegCode32Len-1, DA_C+DA_32
+LABEL_DESC_CODE16:	Descriptor	0, 0ffffh, DA_C
+LABEL_DESC_DATA:	Descriptor	0, DataLen-1, DA_DRW
+LABEL_DESC_STACK:	Descriptor	0, TopOfStack, DA_DRWA+DA_32
+LABEL_DESC_TEST:	Descriptor 0500000h, 0ffffh, DA_DRW
 LABEL_DESC_VIDEO:	Descriptor 0B8000h, 0ffffh, DA_DRW
 
 GdtLen	equ	$-LABEL_GDT
 GdtPtr  dw	GdtLen-1
 	dd	0
 
+SelectorNormal	equ	LABEL_DESC_NORMAL - LABEL_GDT
 SelectorCode32	equ	LABEL_DESC_CODE32 - LABEL_GDT
+SelectorCode16	equ	LABEL_DESC_CODE16 - LABEL_GDT
+SelectorData	equ	LABEL_DESC_DATA - LABEL_GDT
+SelectorStack	equ	LABEL_DESC_STACK - LABEL_GDT
+SelectorTest	equ	LABEL_DESC_TEST - LABEL_GDT
 SelectorVideo	equ	LABEL_DESC_VIDEO - LABEL_GDT
+
+[SECTION .data1]
+ALIGN 32
+[BITS 32]
+LABEL_DATA:
+SPValueInRealMode	dw	0
+PMMessage:		db	"In Protect Mode now.", 0
+OffsetPMMessage		equ	PMMessage - $$
+StrTest:		db	"ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0
+OffsetStrTest		equ	StrTest-$$
+DataLen			equ	$-LABEL_DATA
+
+[SECTION .gs]
+ALIGN 32
+[BITS 32]
+LABEL_STACK:
+	times 512 db 0
+
+TopOfStack	equ	$-LABEL_STACK-1
+
 
 [SECTION .s16]
 [BITS 16]
