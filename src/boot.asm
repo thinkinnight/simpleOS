c80984910178214a4f39e7fff114660b2e33934e
cnyao <yaoqu@163.com>
2016-06-22 01:40:17 -0700

    寻找loader.bin

--- src/boot.asm
+++ src/boot.asm
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ;%define _BOOT_DEBUG_
 
 %ifdef _BOOT_DEBUG_
@@ -6,6 +5,17 @@
 %else
 	org 07c00h	;load from 0000 7c00
 %endif
+
+%ifdef _BOOT_DEBUG_
+BaseOfStack	equ 0100h
+%else
+BaseOfStack	equ 07c00h
+%endif 
+
+BaseOfLoader		equ	09000h
+OffsetOfLoader		equ	0100h
+RootDirSectors		equ	14
+SectorNoOfRootDirectory	equ	19
 
 jmp short LABEL_START
 nop
@@ -31,21 +41,124 @@ BS_VolLab	DB 'SimpleS0.01'; 卷标, 必须 11 个字节
 BS_FileSysType	DB 'FAT12   '	; 文件系统类型, 必须 8个字节  
 
 LABEL_START:
-mov ax, cs	;cs 代码段基地址
-mov ds, ax	;ds 数据段基地址=代码段基地址
-mov es, ax	;es 附加段基地址=代码段基地址
-call DispStr
-jmp $
+	mov ax, cs	;cs 代码段基地址
+	mov ds, ax	;ds 数据段基地址=代码段基地址
+	mov es, ax	;es 附加段基地址=代码段基地址
+	mov ss, ax
+	mov sp, BaseOfStack
+
+	xor ah, ah
+	xor dl, dl
+	int 13h
+
+	mov	word [wSectorNo], SectorNoOfRootDirectory
+LABEL_SEARCH_IN_ROOT_DIR_BEGIN:
+	cmp	word [wRootDirSizeForLoop], 0	;  `. 判断根目录区是不是已经读完
+	jz	LABEL_NO_LOADERBIN		;  /  如果读完表示没有找到 LOADER.BIN
+	dec	word [wRootDirSizeForLoop]	; /
+	mov	ax, BaseOfLoader
+	mov	es, ax			; es <- BaseOfLoader
+	mov	bx, OffsetOfLoader	; bx <- OffsetOfLoader
+	mov	ax, [wSectorNo]		; ax <- Root Directory 中的某 Sector 号
+	mov	cl, 1
+	call	ReadSector
+
+	mov	si, LoaderFileName	; ds:si -> "LOADER  BIN"
+	mov	di, OffsetOfLoader	; es:di -> BaseOfLoader:0100
+	cld
+	mov	dx, 10h
+LABEL_SEARCH_FOR_LOADERBIN:
+	cmp	dx, 0				   ; `. 循环次数控制,
+	jz	LABEL_GOTO_NEXT_SECTOR_IN_ROOT_DIR ;  / 如果已经读完了一个 Sector,
+	dec	dx				   ; /  就跳到下一个 Sector
+	mov	cx, 11
+LABEL_CMP_FILENAME:
+	cmp	cx, 0
+	jz	LABEL_FILENAME_FOUND	; 如果比较了 11 个字符都相等, 表示找到
+	dec	cx
+	lodsb				; ds:si -> al
+	cmp	al, byte [es:di]
+	jz	LABEL_GO_ON
+	jmp	LABEL_DIFFERENT		; 只要发现不一样的字符就表明本 DirectoryEntry
+					; 不是我们要找的 LOADER.BIN
+LABEL_GO_ON:
+	inc	di
+	jmp	LABEL_CMP_FILENAME	; 继续循环
+
+LABEL_DIFFERENT:
+	and	di, 0FFE0h		; else `. di &= E0 为了让它指向本条目开头
+	add	di, 20h			;       |
+	mov	si, LoaderFileName	;       | di += 20h  下一个目录条目
+	jmp	LABEL_SEARCH_FOR_LOADERBIN;    /
+
+LABEL_GOTO_NEXT_SECTOR_IN_ROOT_DIR:
+	add	word [wSectorNo], 1
+	jmp	LABEL_SEARCH_IN_ROOT_DIR_BEGIN
+
+LABEL_NO_LOADERBIN:
+	mov	dh, 2			; "No LOADER."
+	call	DispStr			; 显示字符串
+%ifdef	_BOOT_DEBUG_
+	mov	ax, 4c00h		; `.
+	int	21h			; /  没有找到 LOADER.BIN, 回到 DOS
+%else
+	jmp	$			; 没有找到 LOADER.BIN, 死循环在这里
+%endif
+
+LABEL_FILENAME_FOUND:			; 找到 LOADER.BIN 后便来到这里继续
+	jmp	$			; 代码暂时停在这里
 
 DispStr:
-	mov	ax, BootMessage
+	mov	ax, MessageLength
+	mul	dh
+	add	ax, BootMessage
 	mov	bp, ax			; ES:BP = 串地址
-	mov	cx, 16			; CX = 串长度
+	mov 	ax, ds
+	mov	es, ax
+	mov	cx, MessageLength	; CX = 串长度
 	mov	ax, 01301h		; AH = 13,  AL = 01h
-	mov	bx, 000ch		; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
+	mov	bx, 0007h		; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
 	mov	dl, 0
 	int	10h			; int 10h
 	ret
-BootMessage:		db	"Hello, OS world!"
+
+ReadSector:
+	push bp
+	mov bp, sp
+	sub esp, 2
+
+	mov byte [bp-2], cl
+	push bx
+	mov bl, [BPB_SecPerTrk]
+	div bl
+	inc ah
+	mov cl, ah
+	mov dh, al
+	shr al, 1
+	mov ch, al
+	and dh, 1
+	pop bx
+	mov dl, [BS_DrvNum]
+.GoOnReading:
+	mov ah, 2
+	mov al, byte [bp-2]
+	int 13h
+	jc .GoOnReading
+
+	add esp, 2
+	pop bp
+
+	ret
+
+wRootDirSizeForLoop	dw	RootDirSectors
+wSectorNo		dw	0
+bOdd			db	0
+
+LoaderFileName		db	"LOADER  BIN", 0
+MessageLength		equ	9
+BootMessage:		db	"Booting  "
+Message1:		db	"Ready.   "
+Message2:		db	"No LOADER"
+
 times 	510-($-$$)	db	0	; 填充剩下的空间，使生成的二进制代码恰好为512字节
 dw 	0xaa55				; 结束标志
