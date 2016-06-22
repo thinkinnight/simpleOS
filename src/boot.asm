fa5c3deec7ef5569242c32104156dd923a0e4027
cnyao <yaoqu@163.com>
2016-06-21 20:16:43 -0700

    增加了FAT12文件格式头

--- src/boot.asm
+++ src/boot.asm
@@ -1,4 +1,35 @@
-org 07c00h	;load from 0000 7c00
+;%define _BOOT_DEBUG_
+
+%ifdef _BOOT_DEBUG_
+	org 0100h
+%else
+	org 07c00h	;load from 0000 7c00
+%endif
+
+jmp short LABEL_START
+nop
+
+BS_OEMName	DB	'CnyaoGao'
+BPB_BytsPerSec	DW	512
+BPB_SecPerClus	DB	1
+BPB_RsvdSecCnt	DW	1
+BPB_NumFATs	DB	2
+BPB_RootEntCnt	DW	224
+BPB_TotSec16	DW	2880
+BPB_Media	DB	0xF0
+BPB_FATSz16	DW	9
+BPB_SecPerTrk	DW 18		; 每磁道扇区数
+BPB_NumHeads	DW 2		; 磁头数(面数)
+BPB_HiddSec	DD 0		; 隐藏扇区数
+BPB_TotSec32	DD 0		; wTotalSectorCount为0时这个值记录扇区数
+BS_DrvNum	DB 0		; 中断 13 的驱动器号
+BS_Reserved1	DB 0		; 未使用
+BS_BootSig	DB 29h		; 扩展引导标记 (29h)
+BS_VolID	DD 0		; 卷序列号
+BS_VolLab	DB 'SimpleS0.01'; 卷标, 必须 11 个字节
+BS_FileSysType	DB 'FAT12   '	; 文件系统类型, 必须 8个字节  
+
+LABEL_START:
 mov ax, cs	;cs 代码段基地址
 mov ds, ax	;ds 数据段基地址=代码段基地址
 mov es, ax	;es 附加段基地址=代码段基地址
